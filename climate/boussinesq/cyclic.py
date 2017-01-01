191f50877aa1fb53bef929b97516c36a9e4eb542
Rene Jacobsen <rlj@rloewe.net>
2017-01-01 22:47:28 +0100

    Added cyclic functions

---
+++ climate/boussinesq/cyclic.py
@@ -0,0 +1,49 @@
+def setcyclic_xy(p1, enable_cyclic, onx, nx):
+    """
+    --------------------------------------------------------------
+           set cyclic boundary conditions for 2D array
+    --------------------------------------------------------------
+    """
+    #use main_module
+    #implicit none
+    #integer, intent(in) :: is_,ie_,js_,je_
+    #real*8, intent(inout) :: p1(is_:ie_,js_:je_)
+    #integer :: j,i
+
+    if enable_cyclic_x:
+        for i in xrange(1, onx): #i=1,onx
+            p1[nx+i,:] = p1[i  ,:]
+            p1[1-i,:]  = p1[nx-i+1,:]
+
+def setcyclic_xyz(a, enable_cyclic, onx, nx):
+    """
+    --------------------------------------------------------------
+           set cyclic boundary conditions for 3D array
+    --------------------------------------------------------------
+    """
+    #use main_module
+    #implicit none
+    #integer:: is_,ie_,js_,je_,nz_
+    #real*8 :: a(is_:ie_,js_:je_,nz_)
+    #integer :: k
+    for k in xrange(1, nz): #k=1,nz
+        setcyclic_xy(a[:,:,k], enable_cyclic, onx, nx)
+
+def setcyclic_xyp(np, p1, enable_cyclic, onx, nx):
+    """
+    --------------------------------------------------------------
+           set cyclic boundary conditions for 3D array
+    --------------------------------------------------------------
+    """
+    #use main_module
+    #implicit none
+    #integer:: is_,ie_,js_,je_,np
+    #real*8 :: p1(is_:ie_,js_:je_,np)
+    #integer :: i
+
+    p1[:,:,1 ] = p1[:,:,np-1]
+    p1[:,:,np] = p1[:,:,2]
+    if enable_cyclic_x:
+        for i in xrange(1, onx): #i=1,onx
+            p1[nx+i,:,:] = p1[i  ,:,:]
+            p1[1-i,:,:]  = p1[nx-i+1,:,:]
