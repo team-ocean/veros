f92752e39055099bfab08441b15404c0b33cc2e8
Rene Jacobsen <rlj@rloewe.net>
2017-01-14 20:01:41 +0100

    Check for bohrium

--- climate/__init__.py
+++ climate/__init__.py
@@ -1,4 +1,5 @@
 import importlib
+import numpy as np
 
 from .timer import Timer
 
@@ -10,3 +11,5 @@ __all__ = [
 
 for module in __all__:
     importlib.import_module('.%s' % module, 'climate')
+
+is_bohrium = np.__name__ == 'bohrium'
