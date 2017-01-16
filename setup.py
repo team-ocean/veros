8929580b679d1ef8f9065ac516f55cd29520e47f
René Løwe Jacobsen <rlj@rloewe.net>
2017-01-16 11:58:50 +0100

    The original pyOM does not support numpy 1.12, so for testing purposes we do not either

--- setup.py
+++ setup.py
@@ -6,7 +6,10 @@ setup(
     name = "Climatedsl",
     version = "0.0.1beta",
     packages = find_packages(),
-    install_requires=['numpy', 'scipy'],
+    install_requires = [
+        'numpy<1.12', #NOTE: this is for testing purposes only as the original PyOM does not support numpy 1.12
+        'scipy'
+        ],
 
     author = "René Løwe Jacobsen",
     author_email = "rlj@rloewe.net",
