6f1453de8eab46015914d064cddf2a405f5e223c
Dion Häfner <mail@dionhaefner.de>
2017-01-28 20:20:53 +0100

    started with isoneutral friction methods

--- climate/pyom/isoneutral/friction.py
+++ climate/pyom/isoneutral/friction.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from climate.pyom import numerics
+
 def isoneutral_friction(kbot, nz):
     """
     vertical friction using TEM formalism for eddy driven velocity
@@ -9,94 +11,98 @@ def isoneutral_friction(kbot, nz):
     #real*8 :: diss(is_pe-onx:ie_pe+onx,js_pe-onx:je_pe+onx,nz)
     #real*8 :: aloc(is_pe-onx:ie_pe+onx,js_pe-onx:je_pe+onx,nz)
 
-    a_tri = np.zeros(nz)
-    b_tri = np.zeros(nz)
-    c_tri = np.zeros(nz)
-    d_tri = np.zeros(nz)
-    delta = np.zeros(nz)
+    a_tri = np.zeros(pyom.nz)
+    b_tri = np.zeros(pyom.nz)
+    c_tri = np.zeros(pyom.nz)
+    d_tri = np.zeros(pyom.nz)
+    delta = np.zeros(pyom.nz)
+    diss = np.zeros((pyom.nx+4, pyom.ny+4, pyom.nz))
+    aloc = np.zeros((pyom.nx+4, pyom.ny+4, pyom.nz))
 
-    if enable_implicit_vert_friction:
-        aloc = u[:,:,:,taup1]
+    if pyom.enable_implicit_vert_friction:
+        aloc[...] = pyom.u[:,:,:,pyom.taup1]
     else:
-        aloc = u[:,:,:,tau]
+        aloc[...] = pyom.u[:,:,:,pyom.tau]
 
     # implicit vertical friction of zonal momentum by GM
-    for j in xrange(js_pe-1, je_pe+1): #j=js_pe-1,je_pe
-        for i in xrange(is_pe-1, ie_pe+1): #i=is_pe-1,ie_pe
-            ks = max(kbot[i,j], kbot[i+1,j])
+    for j in xrange(pyom.js_pe-1, pyom.je_pe): #j=js_pe-1,je_pe
+        for i in xrange(pyom.is_pe-1, pyom.ie_pe): #i=is_pe-1,ie_pe
+            ks = max(pyom.kbot[i,j], pyom.kbot[i+1,j])
             if ks > 0:
-                for k in xrange(ks, nz): #k=ks,nz-1
-                    fxa = 0.5*(kappa_gm[i,j,k]+kappa_gm[i+1,j,k])
-                    delta[k] = dt_mom/dzw[k]*fxa*maskU[i,j,k+1]*maskU[i,j,k]
-                delta[nz] = 0.0
+                for k in xrange(ks, pyom.nz-1): #k=ks,nz-1
+                    fxa = 0.5 * (pyom.kappa_gm[i,j,k] + pyom.kappa_gm[i+1,j,k])
+                    delta[k] = pyom.dt_mom / pyom.dzw[k] * fxa * pyom.maskU[i,j,k+1] * pyom.maskU[i,j,k]
+                delta[pyom.nz-1] = 0.0
                 a_tri[ks] = 0.0
-                for k in xrange(ks+1, nz+1): #k=ks+1,nz
-                    a_tri[k] = - delta[k-1]/dzt[k]
-                b_tri[ks] = 1+ delta[ks]/dzt[ks]
-                for k in xrange(ks+1, nz): #k=ks+1,nz-1
-                    b_tri[k] = 1+ delta[k]/dzt[k] + delta[k-1]/dzt[k]
-                b_tri[nz] = 1+ delta[nz-1]/dzt[nz]
-                for k in xrange(ks, nz): #k=ks,nz-1
-                    c_tri[k] = - delta[k]/dzt[k]
-                c_tri[nz] = 0.0
-                d_tri[ks:nz+1] = aloc[i,j,ks:nz+1] #  A u = d
-                u[i,j,ks:,taup1] = numerics.solve_tridiag(a_tri[ks:nz+1],b_tri[ks:nz+1],c_tri[ks:nz+1],d_tri[ks:nz+1])
-                du_mix[i,j,ks:nz+1] = du_mix[i,j,ks:nz+1] + (u[i,j,ks:nz+1,taup1]-aloc[i,j,ks:nz+1])/dt_mom
+                for k in xrange(ks+1, pyom.nz): #k=ks+1,nz
+                    a_tri[k] = -delta[k-1] / pyom.dzt[k]
+                b_tri[ks] = 1 + delta[ks] / pyom.dzt[ks]
+                for k in xrange(ks+1, pyom.nz-1): #k=ks+1,nz-1
+                    b_tri[k] = 1 + delta[k] / pyom.dzt[k] + delta[k-1] / pyom.dzt[k]
+                b_tri[pyom.nz-1] = 1 + delta[pyom.nz-2] / dzt[pyom.nz-1]
+                for k in xrange(ks, pyom.nz-1): #k=ks,nz-1
+                    c_tri[k] = - delta[k] / pyom.dzt[k]
+                c_tri[pyom.nz-1] = 0.0
+                d_tri[ks:nz+1] = aloc[i,j,ks:] #  A u = d
+                pyom.u[i,j,ks:,pyom.taup1] = numerics.solve_tridiag(a_tri[ks:],b_tri[ks:],c_tri[ks:],d_tri[ks:])
+                pyom.du_mix[i,j,ks:] = pyom.du_mix[i,j,ks:] + (pyom.u[i,j,ks:,pyom.taup1] - aloc[i,j,ks:]) / pyom.dt_mom
 
-    if enable_conserve_energy:
+    if pyom.enable_conserve_energy:
         # diagnose dissipation
-        for k in xrange(1, nz): #k=1,nz-1
-            for j in xrange(js_pe-1, je_pe+1): #j=js_pe-1,je_pe
-                for i in xrange(is_pe-1, ie_pe+1): #i=is_pe-1,ie_pe
-                    fxa = 0.5*(kappa_gm[i,j,k]+kappa_gm[i+1,j,k])
-                    flux_top[i,j,k] = fxa*(u[i,j,k+1,taup1]-u[i,j,k,taup1])/dzw[k]*maskU[i,j,k+1]*maskU[i,j,k]
-        for k in xrange(1, nz): # k=1,nz-1
-            for j in xrange(js_pe-1, je_pe+1): # j=js_pe-1,je_pe
-                for i in xrange(is_pe-1, ie_pe+1): # i=is_pe-1,ie_pe
-                    diss[i,j,k] = (u[i,j,k+1,tau]-u[i,j,k,tau])*flux_top[i,j,k]/dzw[k]
-        diss[:,:,nz] = 0.0
-        numerics.ugrid_to_tgrid(is_pe-onx,ie_pe+onx,js_pe-onx,je_pe+onx,nz,diss,diss)
-        K_diss_gm = diss
+        for k in xrange(pyom.nz-1): #k=1,nz-1
+            for j in xrange(pyom.js_pe-1, pyom.je_pe): #j=js_pe-1,je_pe
+                for i in xrange(pyom.is_pe-1, pyom.ie_pe): #i=is_pe-1,ie_pe
+                    fxa = 0.5 * (pyom.kappa_gm[i,j,k] + pyom.kappa_gm[i+1,j,k])
+                    pyom.flux_top[i,j,k] = fxa * (pyom.u[i,j,k+1,pyom.taup1] - pyom.u[i,j,k,pyom.taup1]) \
+                                            / pyom.dzw[k] * pyom.maskU[i,j,k+1] * pyom.maskU[i,j,k]
+        for k in xrange(pyom.nz-1): # k=1,nz-1
+            for j in xrange(pyom.js_pe-1, pyom.je_pe): # j=js_pe-1,je_pe
+                for i in xrange(pyom.is_pe-1, pyom.ie_pe): # i=is_pe-1,ie_pe
+                    diss[i,j,k] = (pyom.u[i,j,k+1,pyom.tau] - pyom.u[i,j,k,pyom.tau]) * pyom.flux_top[i,j,k] / pyom.dzw[k]
+        diss[:,:,-1] = 0.0
+        numerics.ugrid_to_tgrid(diss,diss)
+        pyom.K_diss_gm[...] = diss
 
-    if enable_implicit_vert_friction:
-        aloc = v[:,:,:,taup1]
+    if pyom.enable_implicit_vert_friction:
+        aloc[...] = pyom.v[:,:,:,pyom.taup1]
     else:
-        aloc = v[:,:,:,tau]
+        aloc[...] = pyom.v[:,:,:,pyom.tau]
 
     # implicit vertical friction of meridional momentum by GM
-    for j in xrange(js_pe-1, je_pe+1): # j=js_pe-1,je_pe
-        for i in xrange(is_pe-1, ie_pe+1): # i=is_pe-1,ie_pe
-            ks=max(kbot(i,j),kbot(i,j+1))
+    for j in xrange(pyom.js_pe-1, pyom.je_pe): # j=js_pe-1,je_pe
+        for i in xrange(pyom.is_pe-1, pyom.ie_pe): # i=is_pe-1,ie_pe
+            ks = max(pyom.kbot[i,j], pyom.kbot[i,j+1])
             if ks > 0:
-                for k in xrange(ks, nz): # k=ks,nz-1
-                    fxa = 0.5*(kappa_gm[i,j,k]+kappa_gm[i,j+1,k])
-                    delta[k] = dt_mom/dzw[k]*fxa*maskV[i,j,k+1]*maskV[i,j,k]
-                delta[nz] = 0.0
+                for k in xrange(ks, pyom.nz-1): # k=ks,nz-1
+                    fxa = 0.5 * (pyom.kappa_gm[i,j,k] + pyom.kappa_gm[i,j+1,k])
+                    delta[k] = pyom.dt_mom / pyom.dzw[k] * fxa * pyom.maskV[i,j,k+1] * pyom.maskV[i,j,k]
+                delta[-1] = 0.0
                 a_tri[ks] = 0.0
-                for k in xrange(ks+1, nz+1): # k=ks+1,nz
-                    a_tri[k] = - delta[k-1]/dzt[k]
-                b_tri[ks] = 1+ delta[ks]/dzt[ks]
-                for k in xrange(ks+1, nz): # k=ks+1,nz-1
-                    b_tri[k] = 1+ delta[k]/dzt[k] + delta[k-1]/dzt[k]
-                b_tri[nz] = 1+ delta[nz-1]/dzt[nz]
+                for k in xrange(ks+1, pyom.nz): # k=ks+1,nz
+                    a_tri[k] = - delta[k-1] / pyom.dzt[k]
+                b_tri[ks] = 1 + delta[ks] / pyom.dzt[ks]
+                for k in xrange(ks+1, pyom.nz-1): # k=ks+1,nz-1
+                    b_tri[k] = 1 + delta[k] / pyom.dzt[k] + delta[k-1] / pyom.dzt[k]
+                b_tri[-1] = 1 + delta[-2] / pyom.dzt[-1]
                 for k in xrange(ks, nz-1): # k=ks,nz-1
-                    c_tri[k] = - delta[k]/dzt[k]
-                c_tri[nz] = 0.0
-                d_tri[ks:nz+1] = aloc[i,j,ks:nz+1]
-                v[i,j,ks:,taup1] = numerics.solve_tridiag(a_tri[ks:nz+1],b_tri[ks:nz+1],c_tri[ks:nz+1],d_tri[ks:nz+1])
-                dv_mix[i,j,ks:nz+1] = dv_mix[i,j,ks:nz+1] + (v[i,j,ks:nz,taup1]-aloc[i,j,ks:nz+1])/dt_mom
+                    c_tri[k] = -delta[k] / pyom.dzt[k]
+                c_tri[-1] = 0.0
+                d_tri[ks:] = aloc[i,j,ks:]
+                pyom.v[i,j,ks:,pyom.taup1] = numerics.solve_tridiag(a_tri[ks:],b_tri[ks:],c_tri[ks:],d_tri[ks:])
+                pyom.dv_mix[i,j,ks:] = pyom.dv_mix[i,j,ks:] + (pyom.v[i,j,ks:,pyom.taup1] - aloc[i,j,ks:]) / pyom.dt_mom
 
-    if enable_conserve_energy:
+    if pyom.enable_conserve_energy:
         # diagnose dissipation
-        for k in xrange(1, nz): # k=1,nz-1
-            for j in xrange(js_pe-1, je_pe+1): # j=js_pe-1,je_pe
-                for i in xrange(is_pe-1, ie_pe+1): # i=is_pe-1,ie_pe
-                    fxa = 0.5*(kappa_gm[i,j,k]+kappa_gm[i,j+1,k])
-                    flux_top[i,j,k] = fxa*(v[i,j,k+1,taup1]-v[i,j,k,taup1])/dzw[k]*maskV[i,j,k+1]*maskV[i,j,k]
-        for k in xrange(1, nz): #k=1,nz-1
-            for j in xrange(js_pe-1, je_pe+1): # j=js_pe-1,je_pe
-                for i in xrange(is_pe-1, ie_pe+1): # i=is_pe-1,ie_pe
-                    diss[i,j,k] =(v[i,j  ,k+1,tau]-v[i,j  ,k,tau])*flux_top[i,j  ,k]/dzw[k]
-        diss[:,:,nz] = 0.0
-        numerics.vgrid_to_tgrid(is_pe-onx,ie_pe+onx,js_pe-onx,je_pe+onx,nz,diss,diss)
-        K_diss_gm = K_diss_gm + diss
+        for k in xrange(pyom.nz-1): # k=1,nz-1
+            for j in xrange(pyom.js_pe-1, pyom.je_pe+1): # j=js_pe-1,je_pe
+                for i in xrange(pyom.is_pe-1, pyom.ie_pe+1): # i=is_pe-1,ie_pe
+                    fxa = 0.5 * (pyom.kappa_gm[i,j,k] + pyom.kappa_gm[i,j+1,k])
+                    pyom.flux_top[i,j,k] = fxa * (pyom.v[i,j,k+1,pyom.taup1] - pyom.v[i,j,k,pyom.taup1]) \
+                                            / pyom.dzw[k] * pyom.maskV[i,j,k+1] * pyom.maskV[i,j,k]
+        for k in xrange(pyom.nz-1): #k=1,nz-1
+            for j in xrange(pyom.js_pe-1, pyom.je_pe+1): # j=js_pe-1,je_pe
+                for i in xrange(pyom.is_pe-1, pyom.ie_pe+1): # i=is_pe-1,ie_pe
+                    diss[i,j,k] = (pyom.v[i,j,k+1,pyom.tau] - pyom.v[i,j,k,pyom.tau]) * pyom.flux_top[i,j,k] / pyom.dzw[k]
+        diss[:,:,-1] = 0.0
+        numerics.vgrid_to_tgrid(diss,diss)
+        pyom.K_diss_gm = pyom.K_diss_gm + diss
