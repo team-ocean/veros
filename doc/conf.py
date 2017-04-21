84ea7668d875ede28bce07111143e3b36c06ad2a
Dion Häfner <mail@dionhaefner.de>
2017-04-21 19:09:38 +0200

    [doc] also mock h5py

--- doc/conf.py
+++ doc/conf.py
@@ -23,7 +23,7 @@ sys.path.insert(0, os.path.abspath('3rdparty'))
 
 # Mock packages with C dependencies
 import mock
-MOCK_MODULES = ['pyamg', 'netCDF4']
+MOCK_MODULES = ['pyamg', 'netCDF4', 'h5py']
 sys.modules.update((mod_name, mock.MagicMock()) for mod_name in MOCK_MODULES)
 
 # -- General configuration ------------------------------------------------
