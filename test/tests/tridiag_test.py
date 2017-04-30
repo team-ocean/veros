09321afe124be9fa846df139a1d22cccecde9819
Dion Häfner <mail@dionhaefner.de>
2017-04-30 13:59:51 +0200

    fix tridiag test when using bohrium

--- test/tests/tridiag_test.py
+++ test/tests/tridiag_test.py
@@ -1,4 +1,5 @@
 import numpy as np
+import bohrium as bh
 import sys
 
 from test_base import VerosTest
@@ -13,6 +14,8 @@ class TridiagTest(VerosTest):
         for _ in range(100):
             a, b, c, d = (np.random.randn(self.nz) for _ in range(4))
             out_legacy = self.veros_legacy.fortran.solve_tridiag(a=a,b=b,c=c,d=d,n=self.nz)
+            if self.veros_new.backend_name == "bohrium":
+                a, b, c, d = (bh.array(v) for v in (a,b,c,d))
             out_new = numerics.solve_tridiag(self.veros_new,a,b,c,d)
             if not np.allclose(out_legacy, out_new):
                 return False
