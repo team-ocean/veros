2f53572e2a2face2a2e705fb58189f85ba0d14b8
Dion Häfner <mail@dionhaefner.de>
2017-05-02 21:51:12 +0200

    add cli for tests and benchmarks

--- test/run_benchmarks.py
+++ test/run_benchmarks.py
@@ -1,57 +1,70 @@
+#! /usr/bin/env python
+
 import sys
 import os
 import subprocess
 import multiprocessing
 import time
+import argparse
+import imp
+import math
 
 """
 Runs all Veros benchmarks back to back.
 Expects path to the compiled pyOM2 Fortran library as first command line argument
-(preferably with MPI support).
+(with MPI support).
 """
 
-try:
-    fortran_path = sys.argv[1]
-except IndexError:
-    raise RuntimeError("First command line argument must be path to pyOM Fortran library")
-
-sizes = [10 ** n for n in range(4,7)]
 nproc = multiprocessing.cpu_count()
-benchmark_commands = (
-                      "python {filename} -b numpy -s nx {nx} -s ny {ny} -s nz {nz}",
-                      "BH_STACK=openmp python {filename} -b bohrium -s nx {nx} -s ny {ny} -s nz {nz}",
-                      "BH_STACK=opencl python {filename} -b bohrium -s nx {nx} -s ny {ny} -s nz {nz}",
-                      "python {filename} -f {fortran_lib} -s nx {nx} -s ny {ny} -s nz {nz}",
-                      "mpirun -n {nproc} -- python {filename} -f {fortran_lib} -s nx {nx} -s ny {ny} -s nz {nz}"
-                      )
+benchmark_commands = {
+    "numpy": "{python} {filename} -b numpy -s nx {nx} -s ny {ny} -s nz {nz}",
+    "bohrium": "BH_STACK=openmp {python} {filename} -b bohrium -s nx {nx} -s ny {ny} -s nz {nz}",
+    "bohrium-opencl": "BH_STACK=opencl {python} {filename} -b bohrium -s nx {nx} -s ny {ny} -s nz {nz}",
+    "fortran": "{python} {filename} -f {fortran_lib} -s nx {nx} -s ny {ny} -s nz {nz}",
+    "fortran-mpi": "mpirun -n {nproc} -- {python} {filename} -f {fortran_lib} -s nx {nx} -s ny {ny} -s nz {nz}"
+}
 outfile = "benchmark_{}.log".format(time.time())
 
-with open(outfile, "w") as log:
-    for f in os.listdir("./benchmarks"):
-        if f.endswith("_benchmark.py"):
-            print("Running test {} ... ".format(f))
-            for size in sizes:
-                n = int(size ** (1./3.)) + 1
-                nx = ny = int(2. ** 0.5 * n)
-                nz = n / 2
-                args = {
-                            "filename": os.path.realpath(os.path.join("./benchmarks", f)),
-                            "nproc": nproc,
-                            "fortran_lib": fortran_path,
-                            "nx": nx, "ny": ny, "nz": nz
-                       }
-                for cmd in benchmark_commands:
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Run Veros benchmarks")
+    parser.add_argument("-f", "--fortran-library", type=str)
+    parser.add_argument("-s", "--max-size", type=float, required=True)
+    parser.add_argument("-c", "--component", nargs="*", choices=benchmark_commands.keys(), default=["numpy"])
+    args = parser.parse_args()
 
-                    print(" > " + cmd.format(**args))
-                    start = time.time()
-                    try: # must run each benchmark in its own Python subprocess to reload the Fortran library
-            	        output = subprocess.check_output(cmd.format(**args), shell=True,
-                                                          stderr=subprocess.STDOUT)
-                    except subprocess.CalledProcessError as e:
-                        print(" failed")
-                        print(e.output)
-                        continue
-                    end = time.time()
-                    elapsed = end - start
-                    print(" finished after {}s".format(elapsed))
-                    log.write("{}".format(elapsed))
+    if args.fortran_library:
+        imp.load_dynamic("pyOM_code_MPI", args.fortran_library)
+
+    sizes = [10 ** n for n in range(3,int(math.log10(args.max_size)),1)]
+
+    with open(outfile, "w") as log:
+        for f in os.listdir("./benchmarks"):
+            if f.endswith("_benchmark.py"):
+                print("Running test {} ... ".format(f))
+                for size in sizes:
+                    n = int(size ** (1./3.)) + 1
+                    nx = ny = int(2. ** 0.5 * n)
+                    nz = n / 2
+                    cmd_args = {
+                                "python": sys.executable,
+                                "filename": os.path.realpath(os.path.join("./benchmarks", f)),
+                                "nproc": nproc,
+                                "fortran_lib": args.fortran_library,
+                                "nx": nx, "ny": ny, "nz": nz
+                               }
+                    for comp in args.components:
+                        cmd = benchmark_commands[comp]
+                        print(" > " + cmd.format(**cmd_args))
+                        start = time.time()
+                        try: # must run each benchmark in its own Python subprocess to reload the Fortran library
+                	        output = subprocess.check_output(cmd.format(**cmd_args),
+                                                             shell=True,
+                                                             stderr=subprocess.STDOUT)
+                        except subprocess.CalledProcessError as e:
+                            print(" failed")
+                            print(e.output)
+                            continue
+                        end = time.time()
+                        elapsed = end - start
+                        print(" finished after {}s".format(elapsed))
+                        log.write("{}".format(elapsed))
--- test/run_tests.py
+++ test/run_tests.py
@@ -1,3 +1,5 @@
+#! /usr/bin/env python
+
 import sys
 import os
 import subprocess
@@ -7,36 +9,37 @@ Runs all veros tests back to back and compares the results with the legacy (pyOM
 Expects path to the compiled Fortran library as first command line argument.
 """
 
-try:
-    fortran_path = sys.argv[1]
-except IndexError:
-    raise RuntimeError("First command line argument must be path to pyOM Fortran library")
+if __name__ == "__main__":
+    try:
+        fortran_path = sys.argv[1]
+    except IndexError:
+        raise RuntimeError("First command line argument must be path to pyOM Fortran library")
 
-success, fail = "passed", "failed"
-if sys.stdout.isatty():
-    success = "\x1b[{}m{}\x1b[0m".format("32",success)
-    fail = "\x1b[{}m{}\x1b[0m".format("31",fail)
+    success, fail = "passed", "failed"
+    if sys.stdout.isatty():
+        success = "\x1b[{}m{}\x1b[0m".format("32",success)
+        fail = "\x1b[{}m{}\x1b[0m".format("31",fail)
 
-for f in os.listdir("./tests"):
-    if f.endswith("_test.py"):
-        sys.stdout.write("Running test {} ... ".format(f))
-        sys.stdout.flush()
-        try: # must run each test in its own Python subprocess to reload the Fortran library
-	        output = subprocess.check_output(["python", os.path.join("./tests", f), fortran_path],
-                                              stderr=subprocess.STDOUT)
-        except subprocess.CalledProcessError as e:
-            sys.stdout.write(fail + "\n\n")
-            print(e.output)
-            continue
-        sys.stdout.write(success + "\n")
+    for f in os.listdir("./tests"):
+        if f.endswith("_test.py"):
+            sys.stdout.write("Running test {} ... ".format(f))
+            sys.stdout.flush()
+            try: # must run each test in its own Python subprocess to reload the Fortran library
+    	        output = subprocess.check_output(["python", os.path.join("./tests", f), fortran_path],
+                                                  stderr=subprocess.STDOUT)
+            except subprocess.CalledProcessError as e:
+                sys.stdout.write(fail + "\n\n")
+                print(e.output)
+                continue
+            sys.stdout.write(success + "\n")
 
-        sys.stdout.write("Running test {} with Bohrium ... ".format(f))
-        sys.stdout.flush()
-        try: # must run each test in its own Python subprocess to reload the Fortran library
-	        output = subprocess.check_output(["python", os.path.join("./tests", f), fortran_path, "-b", "bohrium"],
-                                              stderr=subprocess.STDOUT)
-        except subprocess.CalledProcessError as e:
-            sys.stdout.write(fail + "\n\n")
-            print(e.output)
-            continue
-        sys.stdout.write(success + "\n")
+            sys.stdout.write("Running test {} with Bohrium ... ".format(f))
+            sys.stdout.flush()
+            try: # must run each test in its own Python subprocess to reload the Fortran library
+    	        output = subprocess.check_output(["python", os.path.join("./tests", f), fortran_path, "-b", "bohrium"],
+                                                  stderr=subprocess.STDOUT)
+            except subprocess.CalledProcessError as e:
+                sys.stdout.write(fail + "\n\n")
+                print(e.output)
+                continue
+            sys.stdout.write(success + "\n")
