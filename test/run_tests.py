b2e892cd4e8446edb9cc734498fe2285e9da969a
Dion Häfner <mail@dionhaefner.de>
2017-05-05 16:57:27 +0200

    fix path handling in run_tests

--- test/run_tests.py
+++ test/run_tests.py
@@ -9,6 +9,8 @@ Runs all veros tests back to back and compares the results with the legacy (pyOM
 Expects path to the compiled Fortran library as first command line argument.
 """
 
+testdir = os.path.join(os.path.dirname(__file__), os.path.relpath("tests"))
+
 if __name__ == "__main__":
     try:
         fortran_path = sys.argv[1]
@@ -20,12 +22,12 @@ if __name__ == "__main__":
         success = "\x1b[{}m{}\x1b[0m".format("32",success)
         fail = "\x1b[{}m{}\x1b[0m".format("31",fail)
 
-    for f in os.listdir("./tests"):
+    for f in os.listdir(testdir):
         if f.endswith("_test.py"):
             sys.stdout.write("Running test {} ... ".format(f))
             sys.stdout.flush()
             try: # must run each test in its own Python subprocess to reload the Fortran library
-    	        output = subprocess.check_output(["python", os.path.join("./tests", f), fortran_path],
+    	        output = subprocess.check_output(["python", os.path.join(testdir, f), fortran_path],
                                                   stderr=subprocess.STDOUT)
             except subprocess.CalledProcessError as e:
                 sys.stdout.write(fail + "\n\n")
