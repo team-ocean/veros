83fd3a9cc09ae35ed39012a7aada64c174cd370d
Dion Häfner <dion.haefner@nbi.ku.dk>
2019-02-12 13:08:21 +0100

    overhaul opencl tdma solver

--- veros/core/special/tdma_opencl.py
+++ veros/core/special/tdma_opencl.py
@@ -4,6 +4,8 @@ import operator
 
 
 KERNEL = Template('''
+#pragma OPENCL EXTENSION cl_khr_fp64 : enable
+
 kernel void execute(
     const global $DTYPE *a,
     const global $DTYPE *b,
@@ -33,30 +35,18 @@ kernel void execute(
 
 
 def tdma(a, b, c, d, workgrp_size=None):
-    # import pyopencl as cl
     import bohrium as bh
 
     assert a.shape == b.shape == c.shape == d.shape
     assert a.dtype == b.dtype == c.dtype == d.dtype
 
-    # Check that PyOpenCL is installed and that the Bohrium runtime uses the OpenCL backend
-    # if not bh.interop_pyopencl.available():
-    #     raise NotImplementedError("OpenCL not available")
-
-    # Get the OpenCL context from Bohrium
-    # ctx = bh.interop_pyopencl.get_context()
-    # queue = cl.CommandQueue(ctx)
-
     res = bh.empty_like(a)
-    # a_buf, b_buf, c_buf, d_buf, ret_buf = map(bh.interop_pyopencl.get_buffer, (a, b, c, d, res))
 
     cltype = bh.interop_pyopencl.type_np2opencl_str(a.dtype)
     kernel = KERNEL.substitute(SYS_DEPTH=res.shape[-1], DTYPE=cltype)
-    # prg = cl.Program(ctx, kernel).build()
 
     global_size = res.size // res.shape[-1]
-    # prg.execute(queue, [global_size], workgrp_size, a_buf, b_buf, c_buf, d_buf, ret_buf)
     bh.user_kernel.execute(kernel, [a, b, c, d, res],
                            tag="opencl",
-                           param="global_work_size: %d; local_work_size: 1" % global_size)
+                           param={"global_work_size": global_size, "local_work_size": 1)
     return res
