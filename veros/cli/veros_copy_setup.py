5001fd739f5764c693355c23edb9e85a9e31b215
Dion Häfner <dion.haefner@nbi.ku.dk>
2019-04-03 17:57:38 +0200

    more useful handling of --to parameter

--- veros/cli/veros_copy_setup.py
+++ veros/cli/veros_copy_setup.py
@@ -15,18 +15,23 @@ IGNORE_PATTERNS = ["__init__.py", "*.pyc", "__pycache__/"]
 def copy_setup(setup, to=None):
     """Copy a standard setup to another directory"""
     if to is None:
-        to = os.getcwd()
+        to = os.path.join(os.getcwd(), setup)
+
+    parent = os.path.dirname(os.path.realpath(to))
+
+    if not os.path.exists(parent):
+        os.makedirs(parent)
 
     ignore = shutil.ignore_patterns(*IGNORE_PATTERNS)
     shutil.copytree(
-        os.path.join(SETUPDIR, setup), os.path.join(to, setup), ignore=ignore
+        os.path.join(SETUPDIR, setup), to, ignore=ignore
     )
 
 
 @click.command("veros-copy-setup")
 @click.argument("setup", type=click.Choice(SETUPS))
-@click.option("--to", type=click.Path(exists=True, file_okay=False), required=False,
-              default=None, help="Target directory (default: current working directory)")
+@click.option("--to", type=click.Path(dir_okay=False, file_okay=False), required=False,
+              default=None, help="Target directory (default: copy to current working directory)")
 @functools.wraps(copy_setup)
 def cli(*args, **kwargs):
     copy_setup(*args, **kwargs)
