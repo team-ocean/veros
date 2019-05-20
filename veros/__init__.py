7bc6f37b7736ab5a69f863e0363221cf1436b2de
Dion Häfner <dion.haefner@nbi.ku.dk>
2019-05-20 11:53:41 +0200

    drop itt

--- veros/__init__.py
+++ veros/__init__.py
@@ -1,5 +1,3 @@
-from __future__ import absolute_import
-
 from veros._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
--- veros/core/streamfunction/solvers/base.py
+++ veros/core/streamfunction/solvers/base.py
@@ -1,9 +1,7 @@
 from abc import abstractmethod, ABCMeta
 
-from future.utils import with_metaclass
 
-
-class LinearSolver(with_metaclass(ABCMeta)):
+class LinearSolver(metaclass=ABCMeta):
     @abstractmethod
     def __init__(self, vs):
         pass
