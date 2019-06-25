58db0a665973db903e0b6063b5f6a56ab1e2ee76
Roman Nuterman <nuterman@nbi.ku.dk>
2019-06-25 15:36:08 +0200

    fix K_gm_0, eke & idemix settings in bgc_global_four_degree setup

--- veros/setup/bgc_global_4deg/bgc_global_four_degree.py
+++ veros/setup/bgc_global_4deg/bgc_global_four_degree.py
@@ -88,8 +88,24 @@ class GlobalFourDegreeBGC(VerosSetup):
         vs.enable_Prandtl_tke = False
         vs.enable_kappaH_profile = True
 
-        vs.enable_eke = False
-        vs.enable_idemix = False
+        # eke
+        self.K_gm_0 = 1000.0
+        self.enable_eke = False
+        self.eke_k_max = 1e4
+        self.eke_c_k = 0.4
+        self.eke_c_eps = 0.5
+        self.eke_cross = 2.
+        self.eke_crhin = 1.0
+        self.eke_lmin = 100.0
+        self.enable_eke_superbee_advection = False
+        self.enable_eke_isopycnal_diffusion = False
+
+        # idemix
+        self.enable_idemix = False
+        self.enable_idemix_hor_diffusion = False
+        self.enable_eke_diss_surfbot = False
+        self.eke_diss_surfbot_frac = 0.2  #  fraction which goes into bottom
+        self.enable_idemix_superbee_advection = False
 
         vs.eq_of_state_type = 5
 
