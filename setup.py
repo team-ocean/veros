b1e71dca66621f0e3e18902f10e8d56317d98256
Dion Häfner <dion.haefner@nbi.ku.dk>
2020-05-27 12:18:01 +0200

    remove most extra dependencies

--- setup.py
+++ setup.py
@@ -47,11 +47,11 @@ PACKAGE_DATA = ['setup/*/assets.yml', 'setup/*/*.npy', 'setup/*/*.png']
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-with open(os.path.join(here, 'requirements.txt'), encoding='utf-8') as f:
-    install_requires = [f.replace('==', '<=') for f in f.readlines()]
-
 with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
+
+with open(os.path.join(here, 'requirements.txt'), encoding='utf-8') as f:
+    install_requires = [f.replace('==', '<=') for f in f.readlines()]
 
 setup(
     name='veros',
