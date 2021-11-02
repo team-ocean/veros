42cb02d1e2f4ead239d55f168945d99c3257a980
Jonas la Cour <jonaslacour@Jonass-MacBook-Air.local>
2021-11-02 16:48:22 +0100

    formatting

--- benchmarks/external_mode_solver_benchmark.py
+++ benchmarks/external_mode_solver_benchmark.py
@@ -1,6 +1,4 @@
-
 from benchmark_base import benchmark_cli
-
 
 
 @benchmark_cli
@@ -18,50 +16,60 @@ def main(pyom2_lib, timesteps, size):
 
     f = h5py.File(assets)
 
-    
     def solver_state():
-        from veros import(variables as var_mod,settings as settings_mod)
+        from veros import variables as var_mod, settings as settings_mod
+
         default_settings = deepcopy(settings_mod.SETTINGS)
         default_dimensions = deepcopy(var_mod.DIM_TO_SHAPE_VAR)
         var_meta = deepcopy(var_mod.VARIABLES)
-        keys_to_extract = ["boundary_mask","cost","cosu","dxt","dxu","dyt","dyu","hu","hur","hv","hvr","maskT"]
+        keys_to_extract = [
+            "boundary_mask",
+            "cost",
+            "cosu",
+            "dxt",
+            "dxu",
+            "dyt",
+            "dyu",
+            "hu",
+            "hur",
+            "hv",
+            "hvr",
+            "maskT",
+        ]
         variables_subset = {key: var_meta[key] for key in keys_to_extract}
 
         state = VerosState(var_meta=variables_subset, setting_meta=default_settings, dimensions=default_dimensions)
         with state.settings.unlock():
             state.settings.update(
-            nx=f["maskT"].shape[0]-4,
-            ny=f["maskT"].shape[1]-4,
-            nz=f["maskT"].shape[2],
-            dt_tracer = 86400,
-            dt_mom = 1800,
-            enable_free_surface = True,
-            enable_streamfunction = False,
-            enable_cyclic_x= True)
-        
+                nx=f["maskT"].shape[0] - 4,
+                ny=f["maskT"].shape[1] - 4,
+                nz=f["maskT"].shape[2],
+                dt_tracer=86400,
+                dt_mom=1800,
+                enable_free_surface=True,
+                enable_streamfunction=False,
+                enable_cyclic_x=True,
+            )
+
         state.initialize_variables()
         boundary_mask = f["boundary_mask"]
         nisle = boundary_mask.shape[2]
         resize_dimension(state, "isle", nisle)
         with state.variables.unlock():
             for key in keys_to_extract:
-                setattr(state.variables, key, f[key]) 
+                setattr(state.variables, key, f[key])
 
-        return state 
-
+        return state
 
     state = solver_state()
     x0 = allocate(state.dimensions, ("xt", "yt"), fill=0)
-    
+
     def run():
         solver = get_linear_solver(state)
-        solver.solve(state,f["rhs_press"][:],x0)
-
+        solver.solve(state, f["rhs_press"][:], x0)
 
     for i in range(timesteps):
         run()
-
-
 
 
 if __name__ == "__main__":
