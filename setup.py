b0f7062b73de0fcc8eedf331f31cef3e41986b20
Dion Häfner <dion.haefner@nbi.ku.dk>
2021-12-17 13:49:02 +0100

    python 3.10 not quite ready yet

--- setup.py
+++ setup.py
@@ -25,7 +25,6 @@ Programming Language :: Python :: 3
 Programming Language :: Python :: 3.7
 Programming Language :: Python :: 3.8
 Programming Language :: Python :: 3.9
-Programming Language :: Python :: 3.10
 Programming Language :: Python :: Implementation :: CPython
 Topic :: Scientific/Engineering
 Operating System :: Microsoft :: Windows
