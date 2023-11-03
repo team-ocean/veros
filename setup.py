ae266813b75e65b23819168386158dd3f70cc119
Dion Häfner <dion.haefner@simulation.science>
2023-11-03 13:33:59 +0100

    fix?

--- setup.py
+++ setup.py
@@ -26,10 +26,7 @@ Programming Language :: Python :: 3.8
 Programming Language :: Python :: 3.9
 Programming Language :: Python :: 3.10
 Programming Language :: Python :: 3.11
-<<<<<<< HEAD
-=======
 Programming Language :: Python :: 3.12
->>>>>>> main
 Programming Language :: Python :: Implementation :: CPython
 Topic :: Scientific/Engineering
 Operating System :: Microsoft :: Windows
