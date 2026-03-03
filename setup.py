cb65291fc1fcc9ee32dea8226c80dcaefdc19380
Dion Häfner <dion.haefner@simulation.science>
2026-03-03 09:44:13 +0100

    update petsc and python

--- setup.py
+++ setup.py
@@ -25,6 +25,8 @@ Programming Language :: Python :: 3
 Programming Language :: Python :: 3.10
 Programming Language :: Python :: 3.11
 Programming Language :: Python :: 3.12
+Programming Language :: Python :: 3.13
+Programming Language :: Python :: 3.14
 Programming Language :: Python :: Implementation :: CPython
 Topic :: Scientific/Engineering
 Operating System :: Microsoft :: Windows
